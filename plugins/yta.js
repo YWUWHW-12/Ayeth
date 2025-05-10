948235025a00ec7e69df9202432ba436a56f5abe
YWUWHW-12 <rasathanavajitarat@gmail.com>
2025-05-10 20:02:04 +0530

    Update yta.js

--- plugins/yta.js
+++ plugins/yta.js
@@ -70,8 +70,8 @@ const sentMsg = await conn.sendMessage(from, {
                 forwardingScore: 1,
                 isForwarded: true,
                 forwardedNewsletterMessageInfo: {
-                    newsletterJid: '120363416236691740@newsletter',
-                    newsletterName: "*🎵 𝐌𝐄𝐋𝐎𝐃𝐘 𝐕𝐈𝐁𝐄𝐒 🎵*",
+                    newsletterJid: '120363417070951702@newsletter',
+                    newsletterName: "🎬𝐌𝐎𝐕𝐈𝐄 𝐂𝐈𝐑𝐂𝐋𝐄🎬",
                     serverMessageId: 999
                 }
             }
